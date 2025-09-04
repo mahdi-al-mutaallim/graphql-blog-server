7c8cbde8b4ccecab42f998edc3d26d206bf111f6
Mehedi Hasan <thecodermehedi@gmail.com>
2025-09-04 06:06:45 +0600

    fix some query problems

--- src/graphql/resolvers/query/profile.ts
+++ src/graphql/resolvers/query/profile.ts
@@ -4,31 +4,36 @@ import type { Resolver } from "@/types/resolver.js";
 import { jwtHelpers } from "@/utils/jwt.js";
 
 type ProfileQuery = {
-  Query: {
-    profile: Resolver<unknown, Record<string, never>, Payload<Profile | null>>;
-    user: Resolver<Profile, Record<string, never>, Payload<User | null>>;
-  };
+	Query: {
+		profile: Resolver<unknown, Record<string, never>, Payload<(Profile & { user: User }) | null>>;
+	};
+	Profile: {
+		user: Resolver<Profile & { user: User }, Record<string, never>, User>;
+	};
 };
 
 const profileQuery: ProfileQuery = {
-  Query: {
-    profile: async (_parent, _args, { prisma, token }) => {
-      if (!token) {
-        return { success: false, message: "Access denied. Authentication required.", data: null };
-      }
-      const decoded = await jwtHelpers.verifyJwtToken({ token, type: "access" });
-      const result = await prisma.profile.findUnique({ where: { userId: decoded.userId } });
-      return {
-        success: !!result,
-        message: result ? "Profile retrieved successfully." : "Profile not found.",
-        data: result,
-      };
-    },
-    user: async ({ userId }, _args, { prisma }) => {
-      const result = await prisma.user.findUnique({ where: { id: userId } });
-      return { success: !!result, message: result ? "User retrieved successfully." : "User doesn't exists", data: result }
-    }
-  },
+	Query: {
+		profile: async (_parent, _args, { prisma, token }) => {
+			if (!token) {
+				return { success: false, message: "Access denied. Authentication required.", data: null };
+			}
+			const { userId } = await jwtHelpers.verifyJwtToken({ token, type: "access" });
+			const profileWithUser = await prisma.profile.findUnique({ where: { userId }, include: { user: true } });
+			return {
+				success: !!profileWithUser,
+				message: profileWithUser ? "Profile retrieved successfully." : "Profile not found.",
+				data: profileWithUser || null,
+			};
+		},
+	},
+
+	Profile: {
+		user: ({ user }) => {
+			if (!user) throw new Error("User not found");
+			return user;
+		},
+	},
 };
 
 export default profileQuery;
--- src/graphql/resolvers/query/user.ts
+++ src/graphql/resolvers/query/user.ts
@@ -1,30 +1,48 @@
 import type { User } from "@prisma/client.js";
 import type { Payload } from "@/types/payload.js";
+import type { Posts } from "@/types/post.js";
 import type { Resolver } from "@/types/resolver.js";
 import type { Users } from "@/types/user.js";
+import { jwtHelpers } from "@/utils/jwt.js";
 
 type UserQuery = {
 	Query: {
 		users: Resolver<unknown, Record<string, never>, Payload<Users | []>>;
 		user: Resolver<unknown, { id: string }, Payload<User | null>>;
 	};
+	User: {
+		posts: Resolver<User & { posts?: Posts }, Record<string, never>, Posts>;
+	};
 };
 
 const userQuery: UserQuery = {
 	Query: {
 		users: async (_parent, _args, { prisma }) => {
-			const result = await prisma.user.findMany();
+			const result = await prisma.user.findMany({ include: { posts: true } });
 			return {
-				success: true,
+				success: result.length > 0,
 				message: result.length > 0 ? "Users retrieved successfully." : "No users available.",
 				data: result,
 			};
 		},
+
 		user: async (_parent, { id }, { prisma }) => {
-			const result = await prisma.user.findUnique({ where: { id } });
-			return { success: true, message: result ? "User retrieved successfully." : "User not found.", data: result };
+			const result = await prisma.user.findUnique({ where: { id }, include: { posts: true } });
+			return {
+				success: !!result,
+				message: result ? "User retrieved successfully." : "User not found.",
+				data: result || null,
+			};
 		},
-    
+	},
+
+	User: {
+		posts: async ({ posts = [], id }, _args, { token }) => {
+			if (!token) return posts.filter((post) => post.published);
+			const decoded = await jwtHelpers.verifyJwtToken({ token, type: "access" });
+			const isMyProfile = id === decoded.userId;
+			return isMyProfile ? posts : posts.filter((post) => post.published);
+		},
 	},
 };
 
